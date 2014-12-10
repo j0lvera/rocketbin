ef960e72aa7eeccafcca6ba740b15f92a17ee777
Juan Olvera <olverajuanluis@gmail.com>
2014-12-10 02:23:12 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, render_template
+from hashids import Hashids
+import json
+# import jinja2_highlight
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+class MyFlaskApp(Flask):
+    jinja_options = dict(Flask.jinja_options)
+    jinja_options.setdefault('extensions',
+        []).append('jinja2_highlight.HighlightExtension')
+
+app = MyFlaskApp(__name__)
+
+SALT = 'iyVnWkuwcGUXq9vggQtY'
+
+# Mongo Setup
+from bson import ObjectId
+from bson.json_util import dumps
+
+DATABASE_HOST = 'localhost'
+DATABASE_NAME = 'frontbin'
+DATABASE_PORT = 27017
+
+import pymongo
+from pymongo import Connection
+
+connection = Connection(DATABASE_HOST, DATABASE_PORT)
+db = connection[DATABASE_NAME]
+users = db.users
+pastes = db.pastes
+
+# Methods 
+
+def get_new_id():
+    hashids = Hashids(salt=SALT, min_length="16") 
+    id = pastes.find({}).count() + 1
+    return hashids.encrypt(id)
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/paste/save', methods=['POST'])
+def save_paste():
+    error = None
+    print request
+    if request.method == 'POST':
+        _id = get_new_id()
+        code = request.form['code']
+        lang = request.form['lang']
+        pastes.insert({'_id': _id, 'code': code, 'lang': lang })
+        return json.dumps({'success': 200})
+    else:
+        return json.dumps({'error': 'Invalid request'})
+
+@app.route('/paste/<id>')
+def show_paste(id=id):
+    code = pastes.find_one({'_id': id})['code']
+    lang = pastes.find_one({'_id': id})['lang']
+    lexer = get_lexer_by_name(lang, stripall=True)
+    code_result = highlight(code, lexer, HtmlFormatter(linenos=True))
+    print code_result
+    return render_template('code.html', code=code_result, id=id)
+
+# @app.route('/paste/<id>/edit')
+# def edit_paste(id=id):
+#     return None
+
+@app.route('/paste/all')
+def show_all():
+    print pastes.find()
+    return dumps(pastes.find(), indent=2) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,74 @@
+var s,
+  d = document,
+  editor = ace.edit("editor"),
+
+app = {
+  settings: {
+    // Defaults
+    lang: "javascript",
+    theme: "monokai",
+    // UI
+    optionLanguage: d.getElementById('language'),
+    optionTheme: d.getElementById('theme'),
+    form: d.getElementById('form-save-code') 
+  },
+
+  init: function() {
+    s = this.settings;       
+
+    // start app
+    this.setTheme(s.theme);
+    this.setLang(s.lang);
+
+    this.bindUI();
+  },
+
+  bindUI: function() {
+    s.optionLanguage.addEventListener('change', app.changeLang);
+    s.optionTheme.addEventListener('change', app.changeTheme);
+    s.form.addEventListener('submit', app.saveCode);
+  },
+
+  setLang: function(lang) {
+    editor.getSession().setMode("ace/mode/" + lang);
+  },
+
+  setTheme: function(theme) {
+    editor.setTheme("ace/theme/" + theme);
+  },
+  
+  changeLang: function() {
+    value = this.value;
+    s.lang = value;
+    app.setLang(value);
+  },
+
+  changeTheme: function() {
+    value = this.value;
+    s.theme = value;
+    app.setTheme(value);
+  },
+
+  saveCode: function(e) {
+    e.preventDefault();           
+    var code = editor.getValue();
+
+    if (code != '') {
+      $.ajax({
+        url: '/paste/save',
+        data: { code: code, lang: s.lang },
+        type: 'POST',
+        success: function(data) {
+          console.dir(data);
+          console.log("data sended");
+        },
+        error: function(data) {
+          console.dir(data);
+          console.log("something happened");        
+        }
+      });
+    }
+  }
+};
+
+app.init();
