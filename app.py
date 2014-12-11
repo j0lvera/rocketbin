c6636898a0f9df26942ae1f463feaf3484d88bfa
Juan Olvera <olverajuanluis@gmail.com>
2014-12-11 15:33:52 -0600

    procfile updated

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template, redirect, url_for
 from hashids import Hashids
 import json
@@ -81,4 +82,5 @@ def show_raw():
     return dumps(all_entries)
 
 if __name__ == '__main__':
-    app.run()
+    port int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- procfile
+++ procfile
@@ -1,1 +1,2 @@
-web: python app.py
+web: gunicorn app:app
+
