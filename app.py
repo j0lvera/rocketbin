26ba8f7376e9d247d17003e343d0127135b62d06
Juan Olvera <olverajuanluis@gmail.com>
2014-12-11 17:48:30 -0600

    sdf

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pygments.formatters import HtmlFormatter
 
 app = Flask(__name__)
 
-SALT = 'iyVnWkuwcGUXq9vggQtYssS'
+SALT = 'iyVnWkuwcGUXq9vggQtYssxS'
 
 # Mongo Setup
 from bson import ObjectId
