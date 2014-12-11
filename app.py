c14710b41e396fba2789c7654d2d57fb5f7d9c53
Juan Olvera <olverajuanluis@gmail.com>
2014-12-11 15:35:56 -0600

    port updated

--- app.py
+++ app.py
@@ -82,5 +82,5 @@ def show_raw():
     return dumps(all_entries)
 
 if __name__ == '__main__':
-    port int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
