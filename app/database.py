9175753dbe0b242f29a424766295b2aed9db91eb
Juan Olvera <olverajuanluis@gmail.com>
2014-12-27 16:55:20 -0600

    prep for deplpy

--- app/database.py
+++ app/database.py
@@ -6,17 +6,17 @@ import pymongo
 from pymongo import Connection
 
 # Mongo Setup
-DATABASE_HOST = os.getenv('MONGODB_HOST', 'localhost')
-DATABASE_NAME = os.getenv('MONGODB_DATABASE', 'rocketbin-test-4')
-DATABASE_PORT = int(os.getenv('MONGODB_PORT', 27017)) 
-
-# DATABASE_HOST = os.getenv('MONGODB_HOST')
-# DATABASE_NAME = os.getenv('MONGODB_DATABASE')
+# DATABASE_HOST = os.getenv('MONGODB_HOST', 'localhost')
+# DATABASE_NAME = os.getenv('MONGODB_DATABASE', 'rocketbin-test-4')
 # DATABASE_PORT = int(os.getenv('MONGODB_PORT', 27017)) 
+
+DATABASE_HOST = os.getenv('MONGODB_HOST')
+DATABASE_NAME = os.getenv('MONGODB_DATABASE')
+DATABASE_PORT = int(os.getenv('MONGODB_PORT', 27017)) 
 
 connection = Connection(DATABASE_HOST, DATABASE_PORT)
 db = connection[DATABASE_NAME]
-# db.authenticate(os.getenv('MONGODB_USERNAME'), os.getenv('MONGODB_PASSWORD'))
+db.authenticate(os.getenv('MONGODB_USERNAME'), os.getenv('MONGODB_PASSWORD'))
 
 pastes = db.pastes
 
