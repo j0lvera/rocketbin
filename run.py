b40d01bc620c0bf203303e81ebd8478b1433d265
Juan Olvera <olverajuanluis@gmail.com>
2014-12-27 17:26:19 -0600

    prep for deployment

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
 from app import app
-# port = int(os.environ.get('PORT', 5000))
-# app.run(host='0.0.0.0', port=port)
-app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
+# app.run(debug=True)
