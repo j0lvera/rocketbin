e631743243e3272b5f50b87a7735cef4b5984282
Juan Olvera <olverajuanluis@gmail.com>
2015-12-11 14:09:49 -0600

    changed port to PORT

--- run.py
+++ run.py
@@ -2,5 +2,5 @@
 
 import os
 from app import app
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+PORT = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=PORT)
